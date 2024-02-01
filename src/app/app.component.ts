70111f15dd6b29f655bf8d5cefe1fb866abe8d87
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-02-01 18:31:19 +0530

    updated 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,32 @@ import { NgForm } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  @ViewChild('f')signUpform:NgForm
-  topics=['Angular','React','Vue'];
-  defaultQuestion='pet';
-  answer='';
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+  @ViewChild('onSubmit')form:NgForm;
+ // @ViewChild('f')signUpform:NgForm
+  //topics=['Angular','React','Vue'];
+ // defaultQuestion='pet';
+  //answer='';
+  //suggestUserName() {
+   // const suggestedName = 'Superuser';
+  //}
   //Approach:1
   //onSubmit(form:NgForm){
   //console.log(form)
   //}
   //Approach:2
-  onSubmit(){
-   console.log(this.signUpform);
+ // onSubmit(){
+   //console.log(this.signUpform);
 
+  //}
+  firstName:string='';
+  lastName:string='';
+  mail:string='';
+  genders=[
+    {id:'check-male', value:'male', display:'male'},
+    {id:'check-female', value:'female',display:'female'},
+    {id:'check-others', value:'other',display:'other'}
+  ];
+  onFormSubmitted(){
+    console.log(this.form)
   }
 }
