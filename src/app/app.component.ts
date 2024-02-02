107d8b1eb72163d93aabb42ca5b97157f317dbfa
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-02-02 17:38:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Practice_code';
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signupForm:FormGroup
+  forbiddenUsernames=['Chris','Anna'];
+  constructor(){}
+  ngOnInit(): void {
+    this.signupForm= new FormGroup({
+      'userData':new FormGroup({
+        'username': new FormControl(null , Validators.required),
+        'mail':new FormControl(null,[Validators.required,Validators.email])
+      }),
+  
+      'gender':new FormControl('female'),
+      'hobbies':new FormArray([])
+
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm)
+  }
+
+  onAddHobby(){
+    const control=new FormControl(null,Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
 }
